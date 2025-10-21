980a8185070492c133570f50300369921752aa1f
eazy-man <eazyrev15@gmail.com>
2025-10-20 22:51:47 -0400

    Add files via upload

---
+++ semag.js
@@ -0,0 +1,14 @@
+var gamesData = [
+    {
+        title: "Super Mario Bros",
+        image: "images/Super_Mario_Bros._box.png",
+        category: "retro"
+	link: "semag/smb.html"
+    },
+    {
+        title: "Pokemon Red",
+        image: "images/pokemon.jpg",
+        category: "retro"
+	link: "semag/smb.html"
+    }
+];
