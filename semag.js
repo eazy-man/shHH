18d4153d4826aeac5e0354f5208cc34a07d35dad
eazy-man <eazyrev15@gmail.com>
2025-10-21 18:36:48 -0400

    Update semag.js

--- semag.js
+++ semag.js
@@ -2,13 +2,22 @@ var gamesData = [
     {
         title: "Super Mario Bros",
         image: "images/Super_Mario_Bros._box.png",
-        category: "retro"
-	link: "semag/smb.html"
+        link: "semag/smb.html"
     },
     {
-        title: "Pokemon Red",
-        image: "images/pokemon.jpg",
-        category: "retro"
-	link: "semag/smb.html"
+        title: "Sonic The Hedgehog",
+        image: "images/Sonic The Hedgehog (USA, Europe)-image.png",
+        link: "semag/sth.html"
+    },
+    {
+        title: "PaRappa The Rapper",
+        image: "images/PaRappa the Rapper - The Hip Hop Hero (Europe) (En,Fr,De,Es,It)-image.png",
+        link: "semag/ptr.html"
+    },
+    {
+        title: "Um Jammer Lammy",
+        image: "images/Um Jammer Lammy (Europe) (En,Fr,De,Es,It)-image.png",
+        category: "retro",
+        link: "semag/ujl.html"
     }
 ];
