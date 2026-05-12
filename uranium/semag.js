5d1ac6bd21082dff36ba3e51d41258c51f0b88c9
EazyEv15 <138174660+EazyEv15@users.noreply.github.com>
2026-05-12 17:05:55 -0400

    uranium

---
+++ uranium/semag.js
@@ -0,0 +1,653 @@
+var gamesData = [
+    {
+        title: "Super Mario Bros",
+        image: "images/Super_Mario_Bros._box.png",
+        link: "semag/smb.html"
+    },
+    {
+        title: "Sonic The Hedgehog",
+        image: "images/Sonic The Hedgehog (USA, Europe)-image.png",
+        link: "semag/sth.html"
+    },
+    {
+        title: "PaRappa The Rapper",
+        image: "images/PaRappa the Rapper - The Hip Hop Hero (Europe) (En,Fr,De,Es,It)-image.png",
+        link: "semag/ptr.html"
+    },
+    {
+        title: "Um Jammer Lammy",
+        image: "images/Um Jammer Lammy (Europe) (En,Fr,De,Es,It)-image.png",
+        category: "retro",
+        link: "semag/ujl.html"
+    },
+    {
+        title: "Cookie Clicker",
+        image: "images/432bf350e866b2544f9a5cd80de83e0c24f4efddfd7811016c4aa33e48c5df7c.avif",
+        link: "semag/cookieclicker.html"
+    },
+    {
+        title: "Grand Theft Auto",
+        image: "images/Grand Theft Auto (Europe) (En,Fr,De,It) (EDC)-image.png",
+        link: "semag/gta.html"
+    },
+    {
+        title: "Grand Theft Auto 2",
+        image: "images/Grand Theft Auto 2 (Europe) (En,Fr,De,Es,It)-image.png",
+        link: "semag/gta2.html"
+    },
+    {
+        title: "Grand Theft Auto Advance",
+        image: "images/Grand Theft Auto Advance (USA)-image.png",
+        link: "semag/gtaa.html"
+    },
+    {
+        title: "Grand Theft Auto - Chinatown Wars",
+        image: "images/Grand Theft Auto - Chinatown Wars (USA) (En,Fr,De,Es,It)-image.png",
+        link: "semag/gtachina.html"
+    },
+    {
+        title: "Sonic The Hedgehog 2",
+        image: "images/Sonic The Hedgehog 2 (World) (Beta) (1992-08-21)-image.png",
+        link: "semag/sth2.html"
+    },
+    {
+        title: "Sonic The Hedgehog 3",
+        image: "images/Sonic The Hedgehog 3 (USA) (1993-11-20) (Sega Channel)-image.png",
+        link: "semag/sth3.html"
+    },
+    {
+        title: "Crazy Taxi: Catch a Ride",
+        image: "images/maxresdefault (13).jpg",
+        link: "semag/crazytaxi.html"
+    },
+    {
+        title: "Super Mario Bros 3",
+        image: "images/Super Mario Bros. 3 (USA) (Rev 1)-image.png",
+        link: "semag/smb3.html"
+    },
+    {
+        title: "Super Mario Bros 2",
+        image: "images/Super Mario Bros. 2 (USA) (Rev 1)-image.png",
+        link: "semag/smb2.html"
+    },
+    {
+        title: "Super Mario World",
+        image: "images/Super_Mario_World_Coverart.png",
+        link: "semag/smw.html"
+    },
+    {
+        title: "Doom 64",
+        image: "images/Doom_64_box.jpg",
+        link: "semag/doom64.html"
+    },
+    {
+        title: "Doom",
+        image: "images/1200.webp",
+        link: "semag/doom.html"
+    },
+    {
+        title: "Doom 2",
+        image: "images/ss_05f81f237a6370c3c797af951b42291dc747d199.1920x1080.jpg",
+        link: "semag/doom2.html"
+    },
+    {
+        title: "2048",
+        image: "images/2048_OG-logo.jpg.jfif",
+        link: "semag/2048.html"
+    },
+    {
+        title: "2048 Cupcakes",
+        image: "images/66b7c398c3d38a001ed2a2d2.jpg",
+        link: "semag/2048cupcakes.html"
+    },
+    {
+        title: "900719925 4740992",
+        image: "images/9007199254740992.png",
+        link: "semag/9007199254740992.html"
+    },
+    {
+        title: "Bloons Tower Defense",
+        image: "images/71.png",
+        link: "semag/bloonsTD1.html"
+    },
+    {
+        title: "Bloons Tower Defense 2",
+        image: "images/72.png",
+        link: "semag/bloonsTD2.html"
+    },
+    {
+        title: "Bloons Tower Defense 3",
+        image: "images/73.png",
+        link: "semag/bloonsTD3.html"
+    },
+    {
+        title: "Bloons Tower Defense 4",
+        image: "images/74.png",
+        link: "semag/bloonsTD4.html"
+    },
+    {
+        title: "Bloons Tower Defense 5",
+        image: "images/75.png",
+        link: "semag/bloonsTD5.html"
+    },
+    {
+        title: "7 Grand Dad",
+        image: "images/Grand Dad 7 (Unl)-image.jpg",
+        link: "semag/gd7.html"
+    },
+    {
+        title: "Wolfenstein 3D",
+        image: "images/wolfenstein3d.png",
+        link: "semag/wolfenstein3d.html"
+    },
+    {
+        title: "Gran Turismo 2",
+        image: "images/61qihEdj+5L._AC_UF1000,1000_QL80_.jpg",
+        link: "semag/gt2.html"
+    },
+    {
+        title: "Scribblenauts",
+        image: "images/Scribblenauts_cover.jpg",
+        link: "semag/scribblenauts.html"
+    },
+    {
+        title: "Pac-Man",
+        image: "images/puckman-thumb.jpg",
+        link: "semag/puckman.html"
+    },
+    {
+        title: "Journey",
+        image: "images/journey-image.png",
+        link: "semag/journey.html"
+    },
+    {
+        title: "Ms Pac-Man",
+        image: "images/mspacman-thumb.png",
+        link: "semag/mspacman.html"
+    },
+    {
+        title: "Super Mario 64",
+        image: "images/sm64.png",
+        link: "semag/sm64.html"
+    },
+    {
+    	title: "The Spongebob Squarepants Movie",
+    	image: "images/SpongeBob+SquarePants+Movie,+The+(USA)-image.jpg",
+    	link: "semag/tssm.html",
+    },
+    {
+        title: "Spongebob Squarepants Battle For Bikini Bottom",
+        image: "images/SpongeBob+SquarePants+-+Battle+for+Bikini+Bottom+(USA)-image.jpg",
+        link: "semag/bfbb.html"
+    },
+    {
+        title: "Plants Vs. Zombies DS",
+        image: "images/Plants+vs.+Zombies+(USA)-image.jpg",
+        link: "semag/pvz.html"
+    },
+    {
+        title: "Celeste",
+        image: "images/623.png",
+        link: "semag/Celeste.html"
+    },
+    {
+        title: "Happy Wheels",
+        image: "images/624.png",
+        link: "semag/happywheels.html"
+    },
+    {
+        title: "Slope",
+        image: "images/slope-game-v1-f260x260.png",
+        link: "semag/slope.html"
+    },
+    {
+        title: "Lego Star Wars: The Video Game",
+        image: "images/download.jfif",
+        link: "semag/lsw.html"
+    },
+    {
+        title: "Lego Star Wars II: The Original Trilogy",
+        image: "images/61XYZ0RHcOL._AC_UF1000,1000_QL80_.jpg",
+        link: "semag/lsw2.html"
+    },
+    {
+        title: "Tony Hawk's Pro Skater 4",
+        image: "images/thps4.jpg",
+        link: "semag/tonyhawksproskater4.html"
+    },
+    {
+        title: "Tony Hawk's Pro Skater 2",
+        image: "images/thps2.png",
+        link: "semag/tonyhawksproskater2.html"
+    },
+    {
+        title: "Tony Hawk's Pro Skater",
+        image: "images/thps.png",
+        link: "semag/tonyhawksproskater.html"
+    },
+    {
+        title: "Gran Turismo",
+        image: "images/gt.png",
+        link: "semag/granturismo.html"
+    },
+    {
+        title: "Postal",
+        image: "images/postal.png",
+        link: "semag/postal.html"
+    },
+    {
+        title: "Tony Hawk's Pro Skater 3",
+        image: "images/thps3.png",
+        link: "semag/tonyhawksproskater3.html"
+    },
+    {
+        title: "Sonic Advance",
+        image: "images/Sonic Advance.png",
+        link: "semag/sonicadvance.html"
+    },
+    {
+        title: "Sonic Advance 2",
+        image: "images/Sonic Advance 2.png",
+        link: "semag/sonicadvance2.html"
+    },
+    {
+        title: "Sonic Advance 3",
+        image: "images/Sonic Advance 3.png",
+        link: "semag/sonicadvance3.html"
+    },
+    {
+        title: "Sonic Rush",
+        image: "images/Sonic Rush.png",
+        link: "semag/sonicrush.html"
+    },
+    {
+        title: "Sonic Rush Adventure",
+        image: "images/Sonic Rush Adventure.png",
+        link: "semag/sonicrushadventure.html"
+    },
+    {
+        title: "Sonic Colors",
+        image: "images/Sonic Colors.png",
+        link: "semag/soniccolors.html"
+    },
+    {
+        title: "Super Mario Galaxy",
+        image: "images/Super Mario Galaxy.png",
+        link: "semag/smg.html"
+    },
+    {
+        title: "Henry Stickmin Breaking the Bank",
+        image: "images/btb.png",
+        link: "semag/henrybtb.html"
+    },
+    {
+        title: "Henry Stickmin Escaping the Prison",
+        image: "images/etp.png",
+        link: "semag/henryetp.html"
+    },
+    {
+        title: "Henry Stickmin Stealing the Diamond",
+        image: "images/std.png",
+        link: "semag/henrystd.html"
+    },
+    {
+        title: "Henry Stickmin Infiltrating the Airship",
+        image: "images/ita.png",
+        link: "semag/henryita.html"
+    },
+    {
+        title: "Henry Stickmin Fleeing the Complex",
+        image: "images/ftc.png",
+        link: "semag/henryftc.html"
+    },
+    {
+        title: "Duke Nukem 2",
+        image: "images/dn2.png",
+        link: "semag/cldukenukem2.html"
+    },
+    {
+        title: "Tomodachi Collection",
+        image: "images/tomo.png",
+        link: "semag/tomodachi.html"
+    },
+    {
+        title: "3D Pinball Space Cadet",
+        image: "images/3dpinball.png",
+        link: "semag/cl3dpinballspacecadet.html"
+    },
+    {
+        title: "Boxing Random",
+        image: "images/boxing.png",
+        link: "semag/boxingrandom.html"
+    },
+    {
+        title: "Soccer Random",
+        image: "images/soccer.png",
+        link: "semag/soccerrandom.html"
+    },
+    {
+        title: "Volley Random",
+        image: "images/volley.png",
+        link: "semag/volleyrandom.html"
+    },
+    {
+        title: "Basket Random",
+        image: "images/basket.png",
+        link: "semag/basketrandom.html"
+    },
+    {
+        title: "Half-Life",
+        image: "images/halflife.png",
+        link: "semag/half-life.html"
+    },
+    {
+        title: "Drive Mad",
+        image: "images/d.png",
+        link: "semag/drivemad.html"
+    },
+    {
+        title: "Dreadhead Parkour",
+        image: "images/d2.png",
+        link: "semag/dreadhead.html"
+    },
+    {
+        title: "The Impossible Quiz",
+        image: "images/tiq.png",
+        link: "semag/tiq.html"
+    },
+    {
+        title: "Hobo",
+        image: "images/hobo.png",
+        link: "semag/hobo.html"
+    },
+    {
+        title: "Hobo 2: Prison Brawl",
+        image: "images/hobo2.png",
+        link: "semag/hobo2.html"
+    },
+    {
+        title: "Hobo 3: Wanted",
+        image: "images/hobo3.png",
+        link: "semag/hobo3.html"
+    },
+    {
+        title: "Hobo 4: Total War",
+        image: "images/hobo4.png",
+        link: "semag/hobo4.html"
+    },
+    {
+        title: "Hobo 5: Space Brawl",
+        image: "images/hobo5.png",
+        link: "semag/hobo5.html"
+    },
+    {
+        title: "Hobo 6: Hell",
+        image: "images/hobo6.png",
+        link: "semag/hobo6.html"
+    },
+    {
+        title: "Hobo 7: Heaven",
+        image: "images/hobo7.png",
+        link: "semag/hobo7.html"
+    },
+    {
+        title: "Run 3",
+        image: "images/run3.png",
+        link: "semag/clrun3.html"
+    },
+    {
+        title: "Run",
+        image: "images/run.png",
+        link: "semag/run.html"
+    },
+    {
+        title: "Duck Life",
+        image: "images/ducklife.png",
+        link: "semag/ducklife.html"
+    },
+    {
+        title: "Duck Life 2",
+        image: "images/ducklife2.png",
+        link: "semag/ducklife2.html"
+    },
+    {
+        title: "Duck Life 3",
+        image: "images/ducklife3.png",
+        link: "semag/ducklife3.html"
+    },
+    {
+        title: "Duck Life 4",
+        image: "images/ducklife4.png",
+        link: "semag/ducklife4.html"
+    },
+    {
+        title: "Stickman Hook",
+        image: "images/stickmanhook.png",
+        link: "semag/stickmanhook.html"
+    },
+    {
+        title: "Learn to Fly",
+        image: "images/learntofly.png",
+        link: "semag/learntofly.html"
+    },
+    {
+        title: "Learn to Fly 2",
+        image: "images/learntofly2.png",
+        link: "semag/learntofly2.html"
+    },
+    {
+        title: "Fancy Pants Adventure",
+        image: "images/fancypants.png",
+        link: "semag/fancypants.html"
+    },
+    {
+        title: "World's Hardest Game",
+        image: "images/whg.png",
+        link: "semag/whg.html"
+    },
+    {
+        title: "World's Hardest Game 2",
+        image: "images/whg2.png",
+        link: "semag/whg2.html"
+    },
+    {
+        title: "1v1.LOL",
+        image: "images/1v1lol.png",
+        link: "semag/1v1lol.html"
+    },
+    {
+        title: "Google Dino",
+        image: "images/dino.png",
+        link: "semag/dino.html"
+    },
+    {
+        title: "Slither.io",
+        image: "images/snake.png",
+        link: "semag/slitherio.html"
+    },
+    {
+        title: "Hole.io",
+        image: "images/holeio.png",
+        link: "semag/holeio.html"
+    },
+    {
+        title: "Grand Theft Auto: Vice City",
+        image: "images/gtavc.png",
+        link: "semag/vc.html"
+    },
+    {
+        title: "The Simpsons",
+        image: "images/homer.png",
+        link: "semag/ts.html",
+    },
+    {
+        title: "Donkey Kong",
+        image: "images/dk.png",
+        link: "semag/dk.html",
+    },
+    {
+        title: "Super Mario Land",
+        image: "images/sml.png",
+        link: "semag/sml.html",
+    },
+    {
+        title: "All Night Nippon Super Mario Bros.",
+        image: "images/annsmb.png",
+        link: "semag/clannsmb.html",
+    },
+    {
+        title: "Super Mario Sunshine 64",
+        image: "images/sms64.png",
+        link: "semag/clsms64.html",
+    },
+    {
+        title: "Mario Odyssey 64",
+        image: "images/mo64.png",
+        link: "semag/clmo64.html",
+    },
+    {
+        title: "Pokemon Lucid",
+        image: "images/luci.webp",
+        link: "semag/cllucid.html",
+    },
+    {
+        title: "Crash Bandicoot",
+        image: "images/crashbanbicoot.png",
+        link: "semag/clcrashbandicoot.html",
+    },
+    {
+        title: "Pokemon Moon Emerald Extreme Randomizer",
+        image: "images/moon.png",
+        link: "semag/clmoonemeraldextremerandomizer.html",
+    },
+    {
+        title: "Pokemon The Unown King",
+        image: "images/unown.png",
+        link: "semag/cltheunownking.html",
+    },
+    {
+        title: "Pressing Under Pressure",
+        image: "images/pup.png",
+        link: "semag/pup.html",
+    },
+    {
+        title: "NBA Jam T.E",
+        image: "images/nbajamte.png",
+        link: "semag/clnbajamte.html",
+    },
+    {
+        title: "Zuma",
+        image: "images/zuma.png",
+        link: "semag/clzuma.html",
+    },
+    {
+        title: "Suma The Lost Treasure",
+        image: "images/suma.png",
+        link: "semag/clsuma.html",
+    },
+    {
+        title: "Pokemon Scarlet & Violet",
+        image: "images/scarletviolet.png",
+        link: "semag/clscarletviolet.html",
+    },
+    {
+        title: "Getting Over It v1",
+        image: "images/getting.png",
+        link: "semag/gov.html",
+    },
+    {
+        title: "Fallout",
+        image: "images/fallout.png",
+        link: "semag/clfallout.html",
+    },
+    {
+        title: "Audacity",
+        image: "images/audacity.png",
+        link: "semag/audacity/",
+    },
+    {
+        title: "EmulatorJS",
+        image: "images/emujs.png",
+        link: "semag/emujs.html",
+    },
+    {
+        title: "Ruffle",
+        image: "images/ruffle.png",
+        link: "semag/ruffle.html",
+    },
+    {
+        title: "Super Stacker 2",
+        image: "images/superstacker2.png",
+        link: "semag/superstacker2.html",
+    },
+    {
+        title: "Minecraft Pocket Edition",
+        image: "images/mcpe.png",
+        link: "semag/mcpe.html",
+    },
+    {
+        title: "Geometry Dash",
+        image: "images/gd.png",
+        link: "semag/geometry/",
+    },
+    {
+        title: "Animal Crossing",
+        image: "images/ac.png",
+        link: "semag/animalcrossing.html",
+    },
+    {
+        title: "Doom 3",
+        image: "images/Doom3box.png",
+        link: "semag/doom3.html",
+    },
+    {
+        title: "Half-Life Blueshift",
+        image: "images/bshift.png",
+        link: "semag/bshift.html",
+    },
+    {
+        title: "Half-Life Opposing Force",
+        image: "images/opposing.png",
+        link: "semag/opposing.html",
+    },
+    {
+        title: "THREE",
+        image: "images/three.png",
+        link: "semag/three.html",
+    },
+    {
+        title: "Run From Withered Foxy",
+        image: "images/foxy.png",
+        link: "semag/withered.html",
+    },
+    {
+        title: "Plants Vs. Zombies",
+        image: "images/pvz.png",
+        link: "semag/pvz2.html"
+    },
+    {
+        title: "Peggle",
+        image: "images/peggle.png",
+        link: "semag/peggle.html"
+    },
+    {
+        title: "Twiddlefinger",
+        image: "images/twiddlefinger.png",
+        link: "semag/twiddlefinger.html"
+    },
+    {
+        title: "Sonic CD",
+        image: "images/Sonic CD.png",
+        link: "semag/soniccd.html"
+    },
+    {
+        title: "Theme Park",
+        image: "images/themepark.png",
+        link: "semag/clthemeparkpsx.html"
+    },
+    {
+        title: "Windows XP",
+        image: "images/Windows_XP_SP2_Boot_screen.png",
+        link: "semag/windowsxp.html"
+    }
+]
